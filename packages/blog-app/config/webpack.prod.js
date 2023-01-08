1edac5e3063d5b03b81d0a935a07aaf7147b7f09
sokritha-it <sokritha.it@gmail.com>
2023-01-08 17:53:05 +0700

    added publicpath

--- packages/blog-app/config/webpack.prod.js
+++ packages/blog-app/config/webpack.prod.js
@@ -9,6 +9,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/blog/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
